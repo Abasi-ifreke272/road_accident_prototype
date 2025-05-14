c99271f54073acd6e80885cf563a3bcc83e49fe2
sodbridgeacademy <sodbridgeacademy@gmail.com>
2025-05-15 00:15:04 +0100

    added final script2

--- road_safety_app.py
+++ road_safety_app.py
@@ -61,7 +61,7 @@ def accident_severity_prediction_tab(df_merged):
             )
             # updated model in new_folder
             try:
-                rf_model = joblib.load('compressed_rf_model.joblib.xz')  # Ensure this is the correct file for RF model
+                rf_model = joblib.load('compressed_rf_model_v3.joblib.xz')  # Ensure this is the correct file for RF model
                 nn_model = joblib.load('compressed_nn_model.joblib.gz')
                 print(f"Random Forest model type: {type(rf_model)}")
                 print(f"NN model type: {type(nn_model)}")
